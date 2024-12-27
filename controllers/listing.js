5d7abd66aeda435e8d70990ebf5e60ec8d1c5944
Prachi <kshirsagarprachi73@gmail.com>
2024-12-27 13:14:27 +0530

    Add Changes in file

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,7 +1,7 @@
 const Listing=require("../models/listing");
-//const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-//const mapToken=process.env.MAP_TOKEN;
-//const geocodingClient = mbxGeocoding({ accessToken: mapToken});
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken=process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken});
 
   //index route functions
 module.exports.index = async (req, res) => {
@@ -37,45 +37,25 @@ module.exports.showListing =async(req, res) => {
 
  //create Route
   module.exports.createListing = async (req, res, next) => {
+     let response=await geocodingClient.forwardGeocode({
+          query: req.body.listing.location,
+          limit: 1
+        })
+          .send()
+          // .then(response => {
+          //   const match = response.body;
+          // });
     let url=req.file.path;
     let filename=req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner =req.user._id;
     newListing.image={url,filename};
-    await newListing.save();
+    newListing.geometry=response.body.features[0].geometry;
+    let savedListing= await newListing.save();
+    console.log(savedListing);
     req.flash("success","New Listing Created!");
     res.redirect("/listings");
   };
-
-
-//module.exports.createListing = async (req, res, next) => {
-//       let response=await geocodingClient.forwardGeocode({
-//     query: req.body.listing.location,
-//     limit: 1
-//   })
-//     .send();
-//     // .then(response => {
-//     //   const match = response.body;
-//     // })
-    
-    
-    
-//     let url=req.file.path;
-//     let filename=req.file.filename;
-    
-//     const newListing = new Listing(req.body.listing);
-//     newListing.owner= req.user._id;
-//     newListing.image={url,filename};
-
-//     newListing.geometry=response.body.features[0].geometry;
-//    let savedListing= await newListing.save();
-//    console.log(savedListing);
-//     req.flash("success","New Listing created");
-//     res.redirect("/listings");
-    
-    
-//   };  //create route function
-
 
 // edit form route function
 module.exports.renderEditForm = async (req, res) => {
@@ -85,26 +65,25 @@ module.exports.renderEditForm = async (req, res) => {
       req.flash("error","Listing you requested for does not exist");
       res.redirect("/listings");
     }
-    // let originalImageUrl = listing.image.url;
-    // originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_300,w_250") + "?v=" + Date.now();
+     let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/,w_250") + "?v=" + Date.now();
 
-    res.render("listings/edit.ejs", { listing });
-    // res.render("listings/edit.ejs", { listing,originalImageUrl });
+    //res.render("listings/edit.ejs", { listing });
+     res.render("listings/edit.ejs", { listing,originalImageUrl });
   }; 
 
 
 //update route function 
 module.exports.updateListing = async (req, res) => {
-      let { id } = req.params;
-      await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-//     let listing=await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-//     if(typeof req.file !=="undefined"){
+    let { id } = req.params;
+    let listing=await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    if(typeof req.file !=="undefined"){
     
-//     let url=req.file.path;
-//     let filename=req.file.filename;
-//     listing.image={url,filename};
-//     await listing.save();
-  //  }
+    let url=req.file.path;
+    let filename=req.file.filename;
+    listing.image={url,filename};
+    await listing.save();
+  }
     req.flash("success","Listing Updated");
     res.redirect(`/listings/${id}`);
   }; 
--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
+const { required } = require("joi");
 
 const listingSchema = new Schema({
     title: {
@@ -25,6 +26,15 @@ const listingSchema = new Schema({
         type:Schema.Types.ObjectId,
         ref:"User",
     },
+    geometry:{
+    type:String,
+    enum:['Ponit'],
+    required:true,
+    },
+    coordinates:{
+        type: [Number],
+        requires:true,
+    }
 });
 
 listingSchema.post("findOneDelete",async (listing)=>{
